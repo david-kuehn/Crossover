b146d2151fbac9e9137e04d57f83aec5e202fd22
DAVID-PC <dmkuehn6@gmail.com>
2018-01-03 11:46:12 -0600

    Added checks for string and bool variable values. Added some new TokenTypes. Moved work on Tokenizer to new branch.

--- Tokenizer.cs
+++ Tokenizer.cs
@@ -1,5 +1,5 @@
-using System; 
-using System.Collections.Generic; 
+using System;
+using System.Collections.Generic;
 
 public class Tokenizer 
 {
@@ -16,32 +16,87 @@ public class Tokenizer
         //Handle token checks while current position in input is less than the total length
         while (positionInInput <= inputChars.Length)
         {
-            //If 'var' is detected
+            //If variable declaration is detected ('var')
             if (inputChars[positionInInput].Equals('v'))
             {
-                if (inputChars[positionInInput+1].Equals('a'))
+                if (inputChars[positionInInput + 1].Equals('a'))
                     if (inputChars[positionInInput + 2].Equals('r'))
                         if (inputChars[positionInInput + 3].Equals(' '))
                         {
                             //Add a new Variable Declaration token
                             tokensFromInput.Add(new Token(TokenType.VariableDeclaration), "var");
-                            positionInInput += 1;
+                            positionInInput += 3;
                         }
             }
 
-            //Check if character can be or is identifier
+            //If string declaration is detected (single/double quote)
+            else if (inputChars[positionInInput].Equals("'"))
+            {
+                tokensFromInput.Add(CheckForString());
+            }
+
+            //If bool declaration is detected ('true', 'false')
+            else if (inputChars[positionInInput].Equals('t') || inputChars[positionInInput].Equals('f'))
+            {
+                //If the character is a 't'
+                if (inputChars[positionInInput].Equals('t'))
+                {
+                    string checkTrue;
+                    checkTrue = inputChars[positionInInput];
+
+                    //For the next three characters in the input
+                    for (int i = 0; i < 3; i++)
+                    {
+                        //Add them to the checkTrue string
+                        checkTrue += inputChars[positionInInput + i];
+                    }
+
+                    //If they form the word true AND the character after 'true' isn't a letter/digit (makes sure it isn't identifier)
+                    if (checkTrue == "true" && !Char.isLetterOrDigit(inputChars[positionInInput+4]))
+                    {
+                        tokensFromInput.Add(new Token(TokenType.BoolVariable, "true"));
+
+                        //Relocates position to character after 'true' word
+                        positionInInput += 4;
+                    }
+                }
+
+                else if (inputChars[positionInInput].Equals('f'))
+                {
+                    string checkFalse;
+                    checkFalse = inputChars[positionInInput];
+
+                    //For the next four characters in the input
+                    for (int i = 0; i < 4; i++)
+                    {
+                        //Add them to the checkFalse string
+                        checkFalse += inputChars[positionInInput + i];
+                    }
+
+                    //If they form the word false AND the character after 'false' isn't a letter/digit (makes sure it isn't identifier)
+                    if (checkFalse == "false" && !Char.isLetterOrDigit(inputChars[positionInInput + 5]))
+                    {
+                        tokensFromInput.Add(new Token(TokenType.BoolVariable, "false"));
+
+                        //Relocates position to character after 'false' word
+                        positionInInput += 5;
+                    }
+                }
+            }
+
+            //If identifier is detected (letter)
             else if (Char.IsLetter(inputChars[positionInInput]))
             {
                 tokensFromInput.Add(CheckForIdentifier());
             }
 
-            //If no token can be identified, go to next character
+            //If no token can be identified, go to next character (i.e. space)
             else
             {
                 positionInInput += 1;
             }
         }
-
+        
         return tokensFromInput;
     }
 
@@ -78,7 +133,41 @@ public class Tokenizer
         newIdentifierToken.type = TokenType.Identifier;
         return newIdentifierToken;
     }
-    
+
+    Token CheckForString()
+    {
+        Token newIdentifierToken = new Token();
+
+        int position = positionInInput;
+
+        //While current position is less than or equal to the length of the input
+        while (position <= input.Length)
+        {
+            //Current character
+            char character = inputChars[position];
+
+            //If character ends the string
+            if (inputChars[positionInInput].Equals("'"))
+            {
+                //Break out of the loop
+                break;
+            }
+
+            //Add character to new token
+            newIdentifierToken.value += character;
+
+            //Go to next char
+            position += 1;
+        }
+
+        //Update global position to include changes to local position
+        positionInInput += newIdentifierToken.value.Length;
+
+        //Return new token of type StringVariable
+        newIdentifierToken.type = TokenType.StringVariable;
+        return newIdentifierToken;
+    }
+
     Tokenizer(string _input)
     {
         input = _input; 
@@ -117,12 +206,19 @@ public class Token
 
 public enum TokenType 
 {
+    Comment,            // \\
     VariableDeclaration, //var
-    StringVariable,     //" to "
+    StringVariable,     //' to '
     BoolVariable,       //letter AND true, false
     IntVariable,        //digit, NO decimal
     FloatVariable,      //digit, decimal
     Identifier,         //MUST START WITH letter, CAN CONTAIN digit, _ (underscore)
-    Function,           //
-    Operator            //+, -, *, /, =, ==, !=, >=, <=
+    Function,           //exec
+    Operator,           //+, -, *, /, =
+    GreaterThan,        //>
+    LessThan,           //<
+    DblEquals,          //==
+    DoesNotEqual,       //!=
+    GreaterThanOrEqualTo, //>=
+    LessThanOrEqualTo   //<=
 }
